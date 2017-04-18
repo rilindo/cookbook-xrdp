5ed8fa8f829c50e69848f4dcfe8fe5f1b718cdf3
Rilindo Foster <rilindo.foster@monzell.com>
2017-04-17 21:13:05 -0500

    Added platform and version to xrdp::server to clear deprecation errors

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -3,17 +3,18 @@ require 'chefspec/berkshelf'
 
 describe 'xrdp::server' do
   let(:chef_run) {
-    ChefSpec::SoloRunner.new.converge 'xrdp::server'
+    runner = ChefSpec::SoloRunner.new('platform' => 'ubuntu', 'version'=> '12.04')
+    runner.converge('xrdp::server')
   }
-  
+
   it 'installs xrdp server' do
     expect(chef_run).to install_package 'xrdp'
   end
-  
+
   it 'enables xrdp service' do
     expect(chef_run).to enable_service 'xrdp'
   end
-  
+
   it 'starts xrdp service' do
     expect(chef_run).to start_service 'xrdp'
   end
